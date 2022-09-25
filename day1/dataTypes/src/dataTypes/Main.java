4df53bcbd56defc02938763b27148f15b7034b5a
MrEmin <emuhammet.ozdemir@gmail.com>
2022-09-25 20:40:44 +0300

    practice 3

---
+++ day1/dataTypes/src/dataTypes/Main.java
@@ -0,0 +1,21 @@
+package dataTypes;
+
+public class Main {
+
+	public static void main(String[] args) {
+		double sayi = 12.5;
+
+		sayi = -129;
+
+		char karakter = 'A';
+
+		boolean dogruMu = false;
+
+	}
+	/*
+	 * byte, short, int, long tam sayıları tutar. float ve double ise onndalıklı
+	 * sayıları tutar. char tek karakter tutar. boolean ise 1 byte’lık true veya
+	 * false değerini tutar.
+	 */
+
+}
