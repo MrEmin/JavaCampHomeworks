920c83d517566cbc86ba206ee03302989d8b8e6d
MrEmin <emuhammet.ozdemir@gmail.com>
2022-09-26 13:22:11 +0300

    practice 13

---
+++ day1/stringsDemo/src/stringsDemo/Main.java
@@ -0,0 +1,24 @@
+package stringsDemo;
+
+public class Main {
+
+	public static void main(String[] args) {
+
+		String message = "Bugün hava çok güzel.";
+		System.out.println(message);
+
+		System.out.println("Eleman sayýsý : " + message.length());
+		System.out.println("5. eleman : " + message.charAt(4));
+		System.out.println(message.concat(" Yaþasýn!"));
+		System.out.println(message.startsWith("A"));
+		System.out.println(message.endsWith("."));
+
+		char[] characters = new char[5];
+		message.getChars(0, 5, characters, 0);
+
+		System.out.println(characters);
+		System.out.println(message.indexOf("çok"));
+		System.out.println(message.lastIndexOf("a"));
+	}
+
+}
