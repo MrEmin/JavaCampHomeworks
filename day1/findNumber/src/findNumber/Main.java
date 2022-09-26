5e312a0a2ad0b773678085cea729ef2dc2161354
MrEmin <emuhammet.ozdemir@gmail.com>
2022-09-26 15:44:09 +0300

    practice 19 and finished

---
+++ day1/findNumber/src/findNumber/Main.java
@@ -0,0 +1,26 @@
+package findNumber;
+
+import java.util.Iterator;
+
+public class Main {
+
+	public static void main(String[] args) {
+		int[] numbers = new int[] { 1, 2, 5, 7, 9, 0 };
+		int find = 6;
+		boolean isThere = false;
+
+		for (int number : numbers) {
+			if (number == find) {
+				isThere = true;
+				break;
+			}
+		}
+
+		if (isThere) {
+			System.out.println("Sayı Mevcuttur");
+		} else {
+			System.out.println("Sayı Mevcut Değildir.");
+		}
+	}
+
+}
