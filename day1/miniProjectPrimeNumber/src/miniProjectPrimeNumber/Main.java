cf3970e14cce367496d57bf3f1c03f9ef6a179db
MrEmin <emuhammet.ozdemir@gmail.com>
2022-09-26 14:56:20 +0300

    practice 15

--- day1/miniProjectPrimeNumber/src/miniProjectPrimeNumber/Main.java
+++ day1/miniProjectPrimeNumber/src/miniProjectPrimeNumber/Main.java
@@ -3,20 +3,20 @@ package miniProjectPrimeNumber;
 public class Main {
 
 	public static void main(String[] args) {
-		int number = 1;
-		int counter = 0;
+		int number = 2;
+		boolean isPrime = true;
+
+		if (number < 2)
+			isPrime = false;
+
 		for (int i = 2; i < number; i++) {
 			if (number % i == 0) {
-				counter++;
+				isPrime = false;
 			}
 		}
-		
-		if (counter == 0 && number != 1) {
-			System.out.println("Asal Sayý");
-		} else {
-			System.out.println("Asal Sayý Deðil");
-		}
-		
+
+		String result = isPrime ? "Asal Sayýdýr." : "Sayý Asal Deðildir.";
+		System.out.println(result);
 
 	}
 
