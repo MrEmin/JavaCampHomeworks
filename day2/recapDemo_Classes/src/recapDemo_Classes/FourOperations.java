38e0a6890802a886af71801aa53aaa7358df9a7b
MrEmin <emuhammet.ozdemir@gmail.com>
2022-09-26 21:11:54 +0300

    practice 7

---
+++ day2/recapDemo_Classes/src/recapDemo_Classes/FourOperations.java
@@ -0,0 +1,19 @@
+package recapDemo_Classes;
+
+public class FourOperations {
+	public int Plus(int number1, int number2) {
+		return number1 + number2;
+	}
+
+	public int Minus(int number1, int number2) {
+		return number1 - number2;
+	}
+
+	public int Multiplied(int number1, int number2) {
+		return number1 * number2;
+	}
+
+	public double Divide(int number1, int number2) {
+		return number1 / number2;
+	}
+}
---
+++ day2/recapDemo_Classes/src/recapDemo_Classes/Main.java
@@ -0,0 +1,11 @@
+package recapDemo_Classes;
+
+public class Main {
+
+	public static void main(String[] args) {
+		FourOperations fourOperations = new FourOperations();
+		int result = fourOperations.Plus(3, 4);
+		System.out.println("Sonuç: " + result);
+	}
+
+}
