b27722561e3e742affaea4c3b6d3670d5dc443f8
MrEmin <emuhammet.ozdemir@gmail.com>
2022-09-26 19:11:08 +0300

    practice 4

---
+++ day2/classes/src/classes/CustomerManager.java
@@ -0,0 +1,16 @@
+package classes;
+
+public class CustomerManager {
+	// Buraya sadece müşteriyi ilgilendiren metodları yazacağız.
+	public void Add() {
+		System.out.println("Müşteri eklendi");
+	}
+
+	public void Remove() {
+		System.out.println("Müşteri silindi");
+	}
+
+	public void Update() {
+		System.out.println("Müşteri güncellendi");
+	}
+}
