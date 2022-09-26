3cbf53196f227c4972883af2fdce55456d02f5cd
MrEmin <emuhammet.ozdemir@gmail.com>
2022-09-26 19:34:56 +0300

    practice 5

--- day2/classes/src/classes/Main.java
+++ day2/classes/src/classes/Main.java
@@ -3,7 +3,13 @@ package classes;
 public class Main {
 
 	public static void main(String[] args) {
-
+		// referance type
+		CustomerManager customerManager;
+		CustomerManager customerManager2 = new CustomerManager();
+		customerManager = customerManager2; //customerManager'ın refereans numarası artık customerManager2 ile aynı olmuştur.
+		customerManager.Add();
+		customerManager.Remove();
+		customerManager.Update();
 	}
 
 }
